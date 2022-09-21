354e2cc47d5d59014ff5b66039ca613758e33485
Sidharth Raj <sidharth-raj@outlook.com>
2022-09-21 10:52:44 +0530

    removed strict mode

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <React.StrictMode>
-        <Router>
-            <Routes>
-                <Route exact path="/" element={<App />} />
-            </Routes>
-        </Router>
-    </React.StrictMode>
+    <Router>
+        <Routes>
+            <Route exact path="/" element={<App />} />
+        </Routes>
+    </Router>
 );
